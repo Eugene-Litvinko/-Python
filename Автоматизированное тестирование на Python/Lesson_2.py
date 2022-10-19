ce9a2b873457e82a8f448e111b89f3db34f31754
Eugene <eugenelitvinko@gmail.com>
2022-10-19 23:44:47 +0300

    Update Lesson_2.py (add plate)

--- Автоматизированное тестирование на Python/Lesson_2.py
+++ Автоматизированное тестирование на Python/Lesson_2.py
@@ -4,13 +4,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from time import sleep
 
-
 def chek_Availability_porridge():
     porridge = input('Укажите номер каши, которую хотим приготовть?\n1.Гречка\n2.Рис\n3.Овсянка\n4.Другая каша\n5.Каши нету\n')
     return int(porridge)
 
 def prepare_porridge(weight):
     v_castrly = 3000
+    print(f'Количество каши = {weight} грамм')
     print(f'Необходимо взять кастрюлю и налить воды в пропорции 1:3, т.е. {weight*3} мл воды')
     if weight + weight*3 > v_castrly:
         print('Необходимо заменить кастрю')
@@ -20,13 +20,24 @@ def prepare_porridge(weight):
         sleep(1)
 
 def water_and_porridge():
-    print('Сейчас необходимо переодически помешивать кашу, чтобы она не подгорела.\nДелаем это пока не выкипит вся вода!\nОжидание:')
+    print('Сейчас необходимо переодически помешивать кашу, чтобы она не подгорела.\nДелаем это пока не выкипит вся вода!\nОжидание: ')
     for j in range(1,11):
-        print('.', end='')
-        sleep(2)
-    print('Воды не осталось!')
+        print('.', end='') #процесс ожидания испарения воды
+        sleep(1)
+    print('\nВоды не осталось!')
+    print('Нужно выключить плиту.\n')
     return 0
-
+def plate():
+    availability = input('У вас есть тарелка?\n')
+    char_1 = 'н'
+    char_2 = 'n'
+    print(availability.lower()[0])
+    print(availability.lower()[0] == char_1)
+    if availability.lower()[0] == char_2 or char_1: # До конца не разобрался почему не работает корректно (((
+        print('Тогда будем подавать на стол кашу в кастрюле')
+    else:
+        print('Ставим тарелку на стол и ложкой из кастрюли накладываем кашу.')
+    return 0
 
 def main():
     i = True
@@ -43,17 +54,18 @@ def main():
             if porridge != 5:
                 print(f'Здорово! У тебя есть {porridges[porridge]}!')
                 weight = input('Укажите количество каши в граммах:\n')
-                weight = int(weight) #предусмотреть нужно ввод валидных данных
+                weight = int(weight) # предусмотреть нужно ввод валидных данных
                 prepare_porridge(weight)
-                print('Вода закипела!!!\nНебходимо засыпать кашу в кастрюлю с водой, которая стоит на плите!')
+                print('\nВода закипела!!!\nНебходимо засыпать кашу в кастрюлю с водой, которая стоит на плите!\n')
                 water_and_porridge()
+                plate()
+                print('Каша готова! Приятного аппетита')
             elif porridge == 5:
                 print(f'Вам необходимо купить одну из каш и вернуться к нам')
                 i = False
         except:
             print('Ввели неправильный номер. Доступные цифры для ввода: 1,2,3,4,5.\nЗапустите программу снова')
             i = False
-
         i = False
 if __name__ == '__main__':
     print('Вас приветствует алгоритм под названием "Готовка каши"')
